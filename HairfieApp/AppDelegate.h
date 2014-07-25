8d5d7325a18ad95d14722162ef6f7396f5f9bdd5
Ghislain de Juvigny <gdjuvigny@gmail.com>
2014-07-25 18:04:58 +0200

    Initial Commit

---
+++ HairfieApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HairfieApp
+//
+//  Created by Ghislain on 25/07/14.
+//  Copyright (c) 2014 Hairfie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HairfieApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HairfieApp
+//
+//  Created by Ghislain on 25/07/14.
+//  Copyright (c) 2014 Hairfie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
