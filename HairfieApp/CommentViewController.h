3c9d6b715fc3747e705c52dc400adcf97b80c350
Leo <leo@melusyn.com>
2014-08-20 14:00:39 +0200

    merge

---
+++ HairfieApp/CommentViewController.h
@@ -0,0 +1,13 @@
+//
+//  CommentViewController.h
+//  HairfieApp
+//
+//  Created by Leo Martin on 20/08/2014.
+//  Copyright (c) 2014 Hairfie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommentViewController : UIViewController
+
+@end
---
+++ HairfieApp/HoraireTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  HoraireTableViewCell.h
+//  HairfieApp
+//
+//  Created by Leo Martin on 20/08/2014.
+//  Copyright (c) 2014 Hairfie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HoraireTableViewCell : UITableViewCell
+
+@end
