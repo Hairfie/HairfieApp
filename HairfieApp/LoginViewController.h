6158dec298dc666d14a375a90e70d93edb2ff088
Leo <leo@melusyn.com>
2014-08-26 14:46:12 +0200

    adds Login View

---
+++ HairfieApp/LoginViewController.h
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.h
+//  HairfieApp
+//
+//  Created by Leo Martin on 26/08/2014.
+//  Copyright (c) 2014 Hairfie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController <UINavigationControllerDelegate>
+
+@property (nonatomic) IBOutlet UIView *noAccountView;
+
+@end
---
+++ HairfieApp/LoginViewController.m
@@ -0,0 +1,43 @@
+//
+//  LoginViewController.m
+//  HairfieApp
+//
+//  Created by Leo Martin on 26/08/2014.
+//  Copyright (c) 2014 Hairfie. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    _noAccountView.layer.borderColor = [UIColor whiteColor].CGColor;
+    _noAccountView.layer.borderWidth = 0.5;
+    _noAccountView.backgroundColor = [UIColor clearColor];
+    _noAccountView.layer.cornerRadius = 5;
+    _noAccountView.layer.masksToBounds = YES;
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
