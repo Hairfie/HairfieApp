11b6e647ce1c0dcd9bd715729cc98b9a2237742b
Antoine Hérault <antoine.herault@gmail.com>
2014-09-19 13:03:59 +0200

    Add timetable & time window models

---
+++ HairfieApp/TimeWindow.h
@@ -0,0 +1,23 @@
+//
+//  TimeWindow.h
+//  HairfieApp
+//
+//  Created by Antoine Hérault on 19/09/2014.
+//  Copyright (c) 2014 Hairfie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TimeWindow : NSObject
+
+@property (strong, nonatomic) NSString *startTime;
+@property (strong, nonatomic) NSString *endTime;
+@property (strong, nonatomic) NSString *appointmentMode; // OPTIONAL / REQUIRED / DISABLED / UNKNOWN
+
+-(id)initWithDictionary:(NSDictionary *)aDictionary;
+
+-(id)initWithStartTime:(NSString *)aStartTime
+               endTime:(NSString *)anEndTime
+       appointmentMode:(NSString *)anAppointmentMode;
+
+@end
