334d4c207bff59b52144cc41d0a0bfc4f95fc9cc
Leo Martin <leo@hairfie.com>
2014-10-28 11:07:31 +0100

    ui/ux done

--- HairfieApp/TagsTableViewCell.m
+++ HairfieApp/TagsTableViewCell.m
@@ -10,7 +10,9 @@
 #import "Tag.h"
 
 @implementation TagsTableViewCell
-
+{
+    NSInteger indentValue;
+}
 - (void)awakeFromNib {
     // Initialization code
 }
@@ -23,33 +25,66 @@
 
 -(void)setTags:(NSArray*)tags
 {
-    int plusX = 40;
-
+    NSInteger posX = 10;
+    indentValue = 0;
+    NSInteger screenWidth = self.bounds.size.width;
     for (int i = 1; i < [tags count] + 1; i++) {
      
         Tag *tag = [tags objectAtIndex:i - 1];
+        UIButton *button = [[UIButton alloc] init];
         
-        // pos initiale
+        [button setTitle:tag.name forState:UIControlStateNormal];
+        [button addTarget:self action:@selector(buttonClicked:) forControlEvents:UIControlEventTouchUpInside];
+        if (i != 1)
+            posX += indentValue + 15;
+         button.titleLabel.font = [UIFont fontWithName:@"SourceSansPro-Light" size:13];
+        [self getLabelWidth:button.titleLabel];
         
-        int posX = 10 * i;
-        if (posX > 10)
-            posX = (10 + plusX) * i;
-        /////
-        UILabel *label = [[UILabel alloc] init];
-        
-        if (posX != 10)
-        {
-            [label setFrame:CGRectMake(posX, 30, 50, 30)];
-        }
-        else
-            [label setFrame:CGRectMake(posX, 30, 50, 30)];
-        
-        label.text = tag.name;
+      
        
-    
+        
+        CGRect frame;
+        frame.origin.x = posX;
+        frame.origin.y = 40;
+        if (posX + indentValue >= screenWidth)
+        {
+            frame.origin.y += 30;
+            frame.origin.x = 10;
+        }
+        frame.size.width = indentValue + 10;
+        frame.size.height = 27;
+       
+        button.titleLabel.textColor = [UIColor whiteColor];
+        button.backgroundColor = [UIColor salonDetailTab];
+        button.titleLabel.textAlignment = NSTextAlignmentCenter;
+        [button setFrame:frame];
+        button.layer.cornerRadius = 3;
+        button.layer.masksToBounds = YES;
+        button.tag = i;
+       
+        
        // NSLog(@"TAG NAME %@", tag.name);
-        [self addSubview:label];
+        [self addSubview:button];
     }
 }
 
+-(void)getLabelWidth:(UILabel*)aLabel
+{
+    float widthIs =
+    [aLabel.text
+     boundingRectWithSize:aLabel.frame.size
+     options:NSStringDrawingUsesLineFragmentOrigin
+     attributes:@{ NSFontAttributeName:aLabel.font }
+     context:nil]
+    .size.width;
+    
+    indentValue = (int)ceilf(widthIs);
+}
+
+-(IBAction)buttonClicked:(UIButton*)sender
+{
+  
+    
+    NSLog(@"BUTTON CLICKED %ld", sender.tag);
+}
 @end
